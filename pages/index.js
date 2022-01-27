59a85865f87717b7ff3ea34ad78cfa9effca6c1b
venugopal1902 <76394287+venugopal1902@users.noreply.github.com>
2022-01-27 15:06:08 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import requests from '../utils/requests.js';
 import Results from '../components/Results.jsx';
 
 export default function Home({results}) {
-  // console.log(results);
+  console.log(results);
   return (
     <div>
       <Head>
@@ -25,9 +25,10 @@ export default function Home({results}) {
 export async function getServerSideProps(context){
   const genre = context.query.genre;
   const request = await fetch( `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}` ).then((res)=>res?.json());
+  console.log(request);
   return{
     props:{
-      results : request?.results,
+      results : request.results,
     }
   }
 }
